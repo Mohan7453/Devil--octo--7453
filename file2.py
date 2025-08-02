e6fccb9b5cf6745bc01b6aee1b5dedf67e3afc15
Mohan7453 <thakurmonutappal069@gmail.com>
2025-08-02 22:34:40 +0530

    Add files via upload

---
+++ file2.py
@@ -0,0 +1,5 @@
+first_name=input("Enter first name :")
+last_name=input( "Enter last name :")
+full_name=first_name + " "+ last_name
+greeting="welcome to the python program."
+print(f"Hello,{full_name}! Welcome to the python program.")
